f92a04fe44660ac682e453ca0ccdba54f96fac80
Alvin Nuha M <alvinn549@gmail.com>
2025-01-31 21:23:32 +0700

    add favicon and update image paths in index view

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,9 +7,17 @@
 
     <title>AKAD</title>
 
+    <!-- Favicon -->
+    <link rel="icon" href="{{ asset('assets/icon.png') }}" type="image/x-icon">
+
+    <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+    <!-- Font Awesome CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+
+    <!-- Custom CSS -->
     <link rel="stylesheet" href="{{ asset('css/index.css') }}">
 </head>
 
@@ -17,14 +25,14 @@
     <div class="container">
         <div class="auth-container d-md-flex align-items-center justify-content-between">
             <div class="auth-image mt-md-0">
-                <img src="https://images.pexels.com/photos/2033997/pexels-photo-2033997.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-                    class="auth-image-img" alt="">
+                <img src="{{ asset('assets/auth-image.jpeg') }}" class="auth-image-img" alt="">
             </div>
             <div class="auth-form d-flex flex-column h-100">
                 <div class="mt-5">
                     <p class="mb-2 h-1">Sign In</p>
-                    <p class="text-muted mb-2">Welcome to Your Wedding Journey, start planning the most beautiful day of
-                        your life</p>
+                    <p class="text-muted mb-2">
+                        Welcome to Your Wedding Journey, start planning the most beautiful day of your life
+                    </p>
                     <div class="d-flex flex-column ">
                         <p class="text-muted mb-2">Continue with...</p>
                         <div class="d-flex align-items-center justify-content-center">
@@ -49,9 +57,11 @@
         </div>
     </div>
 
+    <!-- J-Query JS -->
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"
         integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 
+    <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
     </script>

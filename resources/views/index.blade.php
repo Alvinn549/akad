70806e4b625269a289499218b0d12c329909c818
Alvin Nuha M <alvinn549@gmail.com>
2025-01-31 21:16:40 +0700

    add home screen page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>AKAD</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <link rel="stylesheet" href="{{ asset('css/index.css') }}">
+</head>
+
+<body>
+    <div class="container">
+        <div class="auth-container d-md-flex align-items-center justify-content-between">
+            <div class="auth-image mt-md-0">
+                <img src="https://images.pexels.com/photos/2033997/pexels-photo-2033997.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+                    class="auth-image-img" alt="">
+            </div>
+            <div class="auth-form d-flex flex-column h-100">
+                <div class="mt-5">
+                    <p class="mb-2 h-1">Sign In</p>
+                    <p class="text-muted mb-2">Welcome to Your Wedding Journey, start planning the most beautiful day of
+                        your life</p>
+                    <div class="d-flex flex-column ">
+                        <p class="text-muted mb-2">Continue with...</p>
+                        <div class="d-flex align-items-center justify-content-center">
+                            <a href="#" class="auth-button me-2">
+                                <span class="fab fa-google mb-2"></span>
+                                <p class="mb-0">Google</p>
+                            </a>
+                            <a href="#" class="auth-button">
+                                <span class="far fa-envelope mb-2"></span>
+                                <p class="mb-0">Email</p>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-auto">
+                    <p class="auth-footer-text text-muted mb-0 mt-md-0 mt-4">By registering you agree with our
+                        <a href="#" class="auth-link">terms and conditions</a> and
+                        <a href="#" class="auth-link">privacy policy</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"
+        integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
